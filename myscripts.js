ce1bdd7a7cf58dce4649ca03040fbef1370a3286
Caz <caz841@gmail.com>
2017-08-10 13:31:32 +0100

    V1 Humanimal Design

--- myscripts.js
+++ myscripts.js
@@ -33,3 +33,18 @@ const FORMBUTTON = document.querySelector("#buyBut");
 const FORMCONTENT = document.querySelector("#buyForm");
 
 FORMBUTTON.addEventListener("click", showForm,false);
+
+ $(function(){
+   $('a[href*=#]:not([href=#])').click(function() {
+       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+         var target = $(this.hash);
+         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+         if (target.length) {
+           $('html,body').animate({
+             scrollTop: (target.offset().top - 135) // adjust this according to your content
+           }, 1000);
+           return false;
+         }
+       }
+   });
+ });
